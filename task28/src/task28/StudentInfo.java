93b53837d73fb1e27ed85bb7877f3137b8e27f25
ABDUL JALEEL MARFANI <ABDUL JALEEL MARFANI@DESKTOP-1POTQHE>
2021-05-26 10:39:50 +0500

    2 commit

---
+++ task28/src/task28/StudentInfo.java
@@ -0,0 +1,28 @@
+package task28;
+
+public class StudentInfo {
+		private String result;
+		StudentInfo(String result){
+		this.result=result;
+		}
+		public String getResult() {
+		return result;
+		}
+		public void setResult(String result) {
+		this.result=result;
+		}
+		public String getName() {
+		return "Usama Marfani";
+		}
+		public String getRollNo() {
+		return "2019-SE-229";
+		}
+		public String getCourseName() {
+		return "Software Construction and Design";
+		}
+		public String getCourseCode() {
+		return "SWE-312";
+		}
+		}
+
+
